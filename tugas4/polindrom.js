64d25002c88b0f458aa4a454f20df8f38d53347e
Hinahinu <sockmoo31@gmail.com>
2023-09-11 13:31:39 +0700

    Polindrom javascript

---
+++ tugas4/polindrom.js
@@ -0,0 +1,12 @@
+function isPalindrome(word) {
+    const reversedWord = word.split('').reverse().join('');
+    
+    return word === reversedWord;
+  }
+  
+  const kata = "racecar";
+  if (isPalindrome(kata)) {
+    console.log(kata + " adalah palindrom.");
+  } else {
+    console.log(kata + " bukan palindrom.");
+  }
